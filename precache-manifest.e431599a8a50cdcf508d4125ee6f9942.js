a9089844bc50b2d93a5d33283a17362beb492ec7
PraneethNamala <praneeth.namala@credencys.com>
2020-06-05 13:10:07 +0530

    Updates

---
+++ precache-manifest.e431599a8a50cdcf508d4125ee6f9942.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75002acbc88d3034ce827609cdf9f82e",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "66b248573d18593333a9",
+    "url": "/React-Todo-App/static/css/main.34e44f0c.chunk.css"
+  },
+  {
+    "revision": "c4dbfbfb381e0acf3bfe",
+    "url": "/React-Todo-App/static/js/2.33e4d0e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo-App/static/js/2.33e4d0e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66b248573d18593333a9",
+    "url": "/React-Todo-App/static/js/main.143a2ba8.chunk.js"
+  },
+  {
+    "revision": "26a0d1af2bcf77eea2a9",
+    "url": "/React-Todo-App/static/js/runtime-main.72f7b68a.js"
+  }
+]);
